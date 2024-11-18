edf5d31654f3bbc25bbe145b4b74b28dad5f3afb
medteamadmins <143048010+medteamadmins@users.noreply.github.com>
2024-11-18 02:44:58 -0500

    Update CylanceUninstall.ps1

--- CylanceUninstall.ps1
+++ CylanceUninstall.ps1
@@ -1,6 +1,9 @@
 Net stop CyOptics
-$CylanceOptics = Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -match "Cylance OPTICS"} | Select-Object -ExpandProperty IdentifyingNumber
-Start-Process "C:\Windows\System32\msiexec.exe" -ArgumentList "/x {6FECE3E5-CA63-4A30-A29D-3600F2057F41} /quiet /l*v C:\uninstall_log.txt" -Wait
+Start-Sleep -Seconds 1
+Start-Process "C:\Windows\System32\msiexec.exe" -ArgumentList "/x {6FECE3E5-CA63-4A30-A29D-3600F2057F41} /quiet /l*v C:\Uninstall_Optics.txt" -Wait
+Start-Sleep -Seconds 2
 Net stop CylanceSVC
+Start-Sleep -Seconds 1
 taskkill /im CylanceUI.exe /f
-Start-Process "C:\Windows\System32\msiexec.exe" -ArgumentList "/x {2E64FC5C-9286-4A31-916B-0D8AE4B22954} /quiet /l*v C:\uninstall_log.txt" -Wait
+Start-Sleep -Seconds 1
+Start-Process "C:\Windows\System32\msiexec.exe" -ArgumentList "/x {2E64FC5C-9286-4A31-916B-0D8AE4B22954} /quiet /l*v C:\uninstall_Protect.txt" -Wait
