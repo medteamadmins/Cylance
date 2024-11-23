7bbb7eb7d794576a2ed52a6431f136bcda3a941a
medteamadmins <143048010+medteamadmins@users.noreply.github.com>
2024-11-23 02:00:35 -0500

    Delete CylanceUninstall.ps1

--- CylanceUninstall.ps1
+++
@@ -1,9 +0,0 @@
-Net stop CyOptics
-Start-Sleep -Seconds 1
-Start-Process "C:\Windows\System32\msiexec.exe" -ArgumentList "/x {6FECE3E5-CA63-4A30-A29D-3600F2057F41} /quiet /l*v C:\Uninstall_Optics.txt" -Wait
-Start-Sleep -Seconds 2
-Net stop CylanceSVC
-Start-Sleep -Seconds 1
-taskkill /im CylanceUI.exe /f
-Start-Sleep -Seconds 1
-Start-Process "C:\Windows\System32\msiexec.exe" -ArgumentList "/x {2E64FC5C-9286-4A31-916B-0D8AE4B22954} /quiet /l*v C:\uninstall_Protect.txt" -Wait
