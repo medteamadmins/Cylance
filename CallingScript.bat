a1779ae6ae479f5150cd6b917cb8466ef40f6961
medteamadmins <143048010+medteamadmins@users.noreply.github.com>
2024-12-05 16:55:13 -0500

    Update CallingScript.bat

--- CallingScript.bat
+++ CallingScript.bat
@@ -2,4 +2,4 @@
 cd "C:\ProgramData\TMT"
 psexec64 -accepteula -h -s sc config cylancesvc start= disabled
 psexec64 -accepteula -h -s sc config cyoptics start= disabled
-psexec64 powershell.exe -ExecutionPolicy Bypass -File "C:\ProgramData\TMT\InstallS1andUninstallCylance.ps1" /accepteula
+psexec64 powershell.exe -ExecutionPolicy Bypass -File "C:\TMTScripts\InstallS1andUninstallCylance.ps1" /accepteula
